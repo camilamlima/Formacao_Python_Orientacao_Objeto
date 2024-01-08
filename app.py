d1b5e48719de1cbee33826f1db2ffe684cc94dff
Camila Lima <camilam.oliveiralima@gmail.com>
2024-01-07 23:23:55 -0300

    Aplicando try e except

--- app.py
+++ app.py
@@ -18,27 +18,38 @@ def exibir_opcoes():
 
 def finalizar_app():
     # os.system('cls')
-    os.system('clear') 
+     
     print('Finalizando o app')
 
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
     # opcao_escolhida = int(opcao_escolhida)
 
-    if opcao_escolhida == 1: 
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2: 
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3: 
-        print('Ativar restaurante')
-    else: 
-        finalizar_app()
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
-
+    
 if __name__ == '__main__':
     main()
