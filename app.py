6807fc65292bca09428978efb62ffb58851c39c3
Camila Lima <camilam.oliveiralima@gmail.com>
2024-01-15 08:41:11 -0300

    Alterando status restaurante

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ def cadastrar_novo_restaurante():
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
     
-
 def listar_restaurantes():
     exibir_subtitulo('Listando os restaurantes\n')
 
@@ -57,6 +56,21 @@ def listar_restaurantes():
 
     voltar_ao_menu_principal()
     
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    
+    restaurante_encontrado = False
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
@@ -68,7 +82,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2: 
             listar_restaurantes()
         elif opcao_escolhida == 3: 
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4: 
             finalizar_app()
         else:
