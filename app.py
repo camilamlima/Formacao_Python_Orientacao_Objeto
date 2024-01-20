68813810cddf69390251e21243968b4f63ff5c14
Camila Lima <camilam.oliveiralima@gmail.com>
2024-01-20 20:47:50 -0300

    Criando novas dosctrings

--- app.py
+++ app.py
@@ -41,6 +41,15 @@ def exibir_subtitulo(texto):
     print()
 
 def cadastrar_novo_restaurante():
+    '''Essa função é responsável por cadastrar um novo restaurante
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Outputs:
+    - Adicionar um novo restaurante a lista de restaurantes
+    '''
     exibir_subtitulo('Cadastro de novos restaurantes\n')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
